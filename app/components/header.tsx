a78d939027b534a86bf2300c2589e1debcd9e103
AzureAD\GautamBedi <gbedi@miningskills.com.au>
2025-09-22 16:14:33 +1000

    => We will be adding the navigation button here

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -4,6 +4,7 @@ const Header = () => {
   return (
     <div className="fixed top-0 left-0 w-full z-10 bg-[#090b13] h-18 flex flex-row justify-between items-center  px-5 md:px-10">
       <img className="w-25" src={"/images/logo.svg"} alt="" />
+      <h1>Login</h1>
     </div>
   );
 };
