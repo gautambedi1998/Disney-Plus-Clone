3a3dd13ebf1b73afd69ba80e37f7d9f7f6c44732
AzureAD\GautamBedi <admin@miningskills.com.au>
2025-11-03 14:32:06 +1000

    => Adding the mobile navigation in the project

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useRouter } from "next/navigation";
 import { signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
 import { auth } from "@/app/Firebase/firebase";
+import { HiMenu, HiX } from "react-icons/hi";
 
 import {
   selectUserName,
@@ -51,6 +52,8 @@ const Header = () => {
 export default Header;
 
 const Navigation = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   const handleLogout = async () => {
     try {
       await signOut(auth).then(() => {
@@ -62,35 +65,112 @@ const Navigation = () => {
   };
 
   return (
-    <nav className="flex gap-6">
-      <Link className="flex flex-row align-middle gap-1.5" href="/home">
-        <img src="/images/home-icon.svg" alt="HOME" />
-        <span>HOME</span>
-      </Link>
-      <Link href="/search">
-        <img src="/images/search-icon.svg" alt="SEARCH" />
-        <span>SEARCH</span>
-      </Link>
-      <Link href="/watchlist">
-        <img src="/images/watchlist-icon.svg" alt="WATCHLIST" />
-        <span>WATCHLIST</span>
-      </Link>
-      <Link href="/movies">
-        <img src="/images/movie-icon.svg" alt="MOVIES" />
-        <span>MOVIES</span>
-      </Link>
-      <Link href="/series">
-        <img src="/images/series-icon.svg" alt="SERIES" />
-        <span>SERIES</span>
-      </Link>
-      <button
-        className="px-12 py-3 border-2 border-white hover:bg-white hover:text-[#040714] hover:cursor-pointer"
-        onClick={() => {
-          handleLogout();
-        }}
-      >
-        Logout
-      </button>
+    <nav>
+      <div className="hidden gap-20 lg:flex flex-row">
+        <Link className="flex flex-row gap-1.5 items-center" href="/home">
+          <img className="w-8" src="/images/home-icon.svg" alt="HOME" />
+          <span className="text-[1.2rem] tracking-wider">HOME</span>
+        </Link>
+        <Link
+          className="flex flex-row align-middle gap-1.5 justify-center items-center"
+          href="/search"
+        >
+          <img className="w-8" src="/images/search-icon.svg" alt="SEARCH" />
+          <span className="text-[1.2rem] tracking-wider">SEARCH</span>
+        </Link>
+        <Link
+          className="flex flex-row align-middle gap-1.5 justify-center items-center"
+          href="/watchlist"
+        >
+          <img
+            className="w-8"
+            src="/images/watchlist-icon.svg"
+            alt="WATCHLIST"
+          />
+          <span className="text-[1.2rem] tracking-wider">WATCHLIST</span>
+        </Link>
+        <Link
+          className="flex flex-row align-middle gap-1.5 justify-center items-center"
+          href="/movies"
+        >
+          <img className="w-8" src="/images/movie-icon.svg" alt="MOVIES" />
+          <span className="text-[1.2rem] tracking-wider">MOVIES</span>
+        </Link>
+        <Link
+          className="flex flex-row align-middle gap-1.5 justify-center items-center"
+          href="/series"
+        >
+          <img className="w-8" src="/images/series-icon.svg" alt="SERIES" />
+          <span className="text-[1.2rem] tracking-wider">SERIES</span>
+        </Link>
+        <button
+          className="px-12 py-3 border-2 border-white hover:bg-white hover:text-[#040714] hover:cursor-pointer text-[1.2rem] tracking-wider"
+          onClick={() => {
+            handleLogout();
+          }}
+        >
+          Logout
+        </button>
+      </div>
+
+      <div className="flex flex-col h-full w-full lg:hidden">
+        <button
+          className="absolute top-4 right-4 z-50"
+          onClick={() => {
+            setIsOpen(!isOpen);
+          }}
+        >
+          {isOpen ? <HiX size={28} /> : <HiMenu size={28} />}
+        </button>
+        {isOpen && (
+          <div className="fixed inset-x-0 mt-8 p-5 z-10 flex flex-col items-center justify-center gap-6 bg-[#090b13] ">
+            <Link className="flex flex-row gap-1.5 items-center" href="/home">
+              <img className="w-8" src="/images/home-icon.svg" alt="HOME" />
+              <span className="text-[1.2rem] tracking-wider">HOME</span>
+            </Link>
+            <Link
+              className="flex flex-row align-middle gap-1.5 justify-center items-center"
+              href="/search"
+            >
+              <img className="w-8" src="/images/search-icon.svg" alt="SEARCH" />
+              <span className="text-[1.2rem] tracking-wider">SEARCH</span>
+            </Link>
+            <Link
+              className="flex flex-row align-middle gap-1.5 justify-center items-center"
+              href="/watchlist"
+            >
+              <img
+                className="w-8"
+                src="/images/watchlist-icon.svg"
+                alt="WATCHLIST"
+              />
+              <span className="text-[1.2rem] tracking-wider">WATCHLIST</span>
+            </Link>
+            <Link
+              className="flex flex-row align-middle gap-1.5 justify-center items-center"
+              href="/movies"
+            >
+              <img className="w-8" src="/images/movie-icon.svg" alt="MOVIES" />
+              <span className="text-[1.2rem] tracking-wider">MOVIES</span>
+            </Link>
+            <Link
+              className="flex flex-row align-middle gap-1.5 justify-center items-center"
+              href="/series"
+            >
+              <img className="w-8" src="/images/series-icon.svg" alt="SERIES" />
+              <span className="text-[1.2rem] tracking-wider">SERIES</span>
+            </Link>
+            <button
+              className="px-12 py-3 border-2 border-white hover:bg-white hover:text-[#040714] hover:cursor-pointer text-[1.2rem] tracking-wider"
+              onClick={() => {
+                handleLogout();
+              }}
+            >
+              Logout
+            </button>
+          </div>
+        )}
+      </div>
     </nav>
   );
 };
@@ -116,7 +196,7 @@ const LoginButton = () => {
   return (
     <button
       onClick={() => handleLogin()}
-      className="px-12 py-3 border-2 border-white hover:bg-white hover:text-[#040714] hover:cursor-pointer"
+      className="px-12 py-3 border-2 border-white hover:bg-white hover:text-[#040714] hover:cursor-pointer text-[1.2rem] tracking-wider"
     >
       Login
     </button>
