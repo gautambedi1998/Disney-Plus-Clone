4bc382023b21305b026d9dfe9caebd0d100571a8
AzureAD\GautamBedi <admin@miningskills.com.au>
2025-11-03 11:06:26 +1000

    => Added the login functionality for the app.

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,8 +1,10 @@
 "use client";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
+import { signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
+import { auth } from "@/app/Firebase/firebase";
 
 import {
   selectUserName,
@@ -12,11 +14,36 @@ import {
 } from "../features/user/userSlice";
 
 const Header = () => {
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const userName = useSelector(selectUserName);
+  const userPhoto = useSelector(selectUserPhoto);
+  useEffect(() => {
+    const authChange = auth.onAuthStateChanged((user) => {
+      if (user) {
+        dispatch(
+          setUserLoginDetails({
+            name: user.displayName,
+            email: user.email,
+            photo: user.photoURL,
+          })
+        );
+        router.push("./home");
+      } else {
+        dispatch(setSignoutState());
+        router.push("./");
+      }
+    });
+    return () => {
+      console.log("---------->" + userName);
+      authChange();
+    };
+  }, [dispatch]);
+
   return (
     <div className="fixed top-0 left-0 w-full z-10 bg-[#090b13] h-18 flex flex-row justify-between items-center  px-5 md:px-10">
       <img className="w-25" src={"/images/logo.svg"} alt="Disney +" />
-      <Navigation />
-      <LoginButton />
+      {userName ? <Navigation /> : <LoginButton />}
     </div>
   );
 };
@@ -24,9 +51,19 @@ const Header = () => {
 export default Header;
 
 const Navigation = () => {
+  const handleLogout = async () => {
+    try {
+      await signOut(auth).then(() => {
+        console.log("Signout Successful");
+      });
+    } catch (error) {
+      return "Error in Handle Logout" + error;
+    }
+  };
+
   return (
     <nav className="flex gap-6">
-      <Link className="flex flex-row align-middle gap-1.5" href="/">
+      <Link className="flex flex-row align-middle gap-1.5" href="/home">
         <img src="/images/home-icon.svg" alt="HOME" />
         <span>HOME</span>
       </Link>
@@ -46,20 +83,41 @@ const Navigation = () => {
         <img src="/images/series-icon.svg" alt="SERIES" />
         <span>SERIES</span>
       </Link>
+      <button
+        className="px-12 py-3 border-2 border-white hover:bg-white hover:text-[#040714] hover:cursor-pointer"
+        onClick={() => {
+          handleLogout();
+        }}
+      >
+        Logout
+      </button>
     </nav>
   );
 };
 
-const handleLogin = async () => {
-  try {
-  } catch (error) {
-    return "Error in handle login" + error;
-  }
-};
-
 const LoginButton = () => {
+  const handleLogin = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+      signInWithPopup(auth, provider)
+        .then((result) => {
+          const credential = GoogleAuthProvider.credentialFromResult(result);
+          const token = credential?.accessToken;
+          console.log(token);
+          const user = result.user;
+        })
+        .catch((error) => {
+          console.log("Error in handleLogin" + error);
+        });
+    } catch (error) {
+      return "Error in Handle Login" + error;
+    }
+  };
   return (
-    <button className="px-12 py-3 border-2 border-white hover:bg-white hover:text-[#040714] hover:cursor-pointer">
+    <button
+      onClick={() => handleLogin()}
+      className="px-12 py-3 border-2 border-white hover:bg-white hover:text-[#040714] hover:cursor-pointer"
+    >
       Login
     </button>
   );
