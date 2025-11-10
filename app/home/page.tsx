691d2ad1a7a51760db54c5682517876b705c5b3c
AzureAD\GautamBedi <admin@miningskills.com.au>
2025-11-10 15:03:33 +1000

    => Added the background for home page => Added the carousel file

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="w-screen h-screen bg-[url('/images/home-background.png')] bg-cover bg-no-repeat">
+      HomePage
+    </div>
+  );
 };
 
 export default HomePage;
