78faaef4eda0fe37423e1d86108965ba6453a43b
AzureAD\GautamBedi <admin@miningskills.com.au>
2025-11-11 13:06:19 +1000

    => Added the layout for the carousel

--- app/components/carousel.tsx
+++ app/components/carousel.tsx
@@ -1,19 +1,38 @@
 "use client";
 import React, { useEffect, useRef, useState } from "react";
+import { bannerImages } from "../constants/index";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const Carousel = () => {
-  const bannerRef = useRef([]);
+  const bannerRef = useRef<HTMLDivElement[]>([]);
+  const sliderRef = useRef<HTMLDivElement>(null);
 
   const [bannerImage, setBannerImage] = useState({
     bannerId: 0,
     isLastBanner: false,
   });
 
+  // useGSAP(() =>
+  //   gsap.to(sliderRef.current, {
+  //     x: `-${100 * 1}%`,
+  //     duration: 2,
+  //     ease: "power2.inOut",
+  //   })
+  // );
+
   return (
-    <div className="flex flex-row ">
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
+    <div
+      className="flex flex-row"
+      style={{ width: `${bannerImages.length * 100}%` }}
+    >
+      {bannerImages.map((element, i) => (
+        <div key={element.id} id="slider" className="w-[80vw]">
+          {element.id}
+          <img src={element.source} className="w-full h-full object-cover" />
+          <span className="absolute h-full w-full rounded-full caret-amber-200 "></span>
+        </div>
+      ))}
     </div>
   );
 };
